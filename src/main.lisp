e9a5c3d47a3562aa7904c125826658f3f415b96b
Jani Hakala <jjhakala@gmail.com>
2021-11-17 00:16:08 +0200

    Replace :use with :import-from

--- src/main.lisp
+++ src/main.lisp
@@ -12,10 +12,9 @@
 ;;  You should have received a copy of the GNU General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
-(require :iterate)
-
 (defpackage :cl-sudoku
-  (:use :cl :alexandria-2 :iterate)
+  (:import-from :iterate :collect :finally :for :in :iter :reducing)
+  (:use :cl :alexandria-2)
   (:export main print-grid))
 
 (in-package :cl-sudoku)
